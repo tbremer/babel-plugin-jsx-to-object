8917c4ab0699904cf1a66e4c03ec974a87b39a1a
Thomas Bremer <tom@tbremer.com>
2016-04-01 03:18:07 -0500

    add test cases

--- test/test.js
+++ test/test.js
@@ -2,25 +2,47 @@ import assert from 'assert';
 import { transform } from 'babel-core';
 import plugin from '../src';
 
-describe('FIXTURE SHOUJLD DO STUFF', () => {
-  // it('should do things too', () => {
-  //   const FIXTURE = `<div />`;
+const transformToJSON (str) => {};
+
+describe('babel-plugin-jsx-to-object', () => {
+
+  /**
+   * SINGLE ELEMENTS
+   */
+  it('TRANSFORM SINGLE ELEMENTS', () => {});
+
+  /**
+   * ELEMENT WITH TEXT
+   */
+  it('TRANSFORM ELEMENT WITH TEXT', () => {});
+
+  /**
+   * SINGLE ELEMENT WITH ATTRIBUTES
+   */
+  it('TRANSFORM SINGLE ELEMENT WITH ATTRIBUTES', () => {});
+
+  /**
+   * SINGLE ELEMENT WITH FUNCTION ATTRIBUTE
+   */
+  it('TRANSFORM SINGLE ELEMENT WITH FUNCTION ATTRIBUTE', () => {});
+
+  /**
+   * SINGLE ELEMENT WITH BOOLEAN ATTRIBUTE
+   */
+  it('TRANSFORM SINGLE ELEMENT WITH BOOLEAN ATTRIBUTE', () => {});
+
+  /**
+   * ELEMENT WITH CHILD
+   */
+  it('TRANSFORM ELEMENT WITH CHILD', () => {});
+
+  /**
+   * ELEMENT WITH MANY CHILDREN
+   */
+  it('TRANSFORM ELEMENT WITH MANY CHILDREN', () => {});
+  //   const FIXTURE = `<div booelan={this.props.foo}><section>{this.props.children}</section></div>`;
   //   let code = transform(FIXTURE, { plugins: [plugin] }).code;
 
   //   console.log(code);
   // });
-
-  // it('should do things too', () => {
-  //   const FIXTURE = `<div>hello world</div>`;
-  //   let code = transform(FIXTURE, { plugins: [plugin] }).code;
-
-  //   console.log(code);
-  // });
-
-  it('should do other neat things', () => {
-    const FIXTURE = `<div booelan={this.props.foo}><section>{this.props.children}</section></div>`;
-    let code = transform(FIXTURE, { plugins: [plugin] }).code;
-
-    console.log(code);
-  });
 })
