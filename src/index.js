9a9aaf9f2b55da1d811d84018d48c8494a7135d8
Tom Bremer <tom@tbremer.com>
2016-04-02 11:25:04 -0500

    Add dist/ to git ignore, fix test watching, add generateElement to Plugin, add tests

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 export default function ({types: t}) {
+  const generateElement = (path) => {
+    const OPENING_ELEMENT = path.get('openingElement'),
+    elementName = OPENING_ELEMENT.node.name.name,
+    attributes = OPENING_ELEMENT.get('attributes').map((attr) => {
+      return t.ObjectProperty(
+        t.StringLiteral(attr.get('name').node.name),
+        (attr.node.value === null ? t.BooleanLiteral(true) : attr.get('value'))
+      );
+    });
+
+    return t.ObjectExpression([
+      t.ObjectProperty(t.StringLiteral('elementName'), t.StringLiteral(elementName)),
+      t.ObjectProperty(t.StringLiteral('attributes'), t.ObjectExpression(attributes))
+    ]);
+  };
+
   return {
     inherits: require('babel-plugin-syntax-jsx'),
     visitor: {
       JSXElement: function(path) {
-        console.log('ELEMENT');
-        console.log(this);
-        let type = path.get('openingElement').get('name').node.name;
+        console.log('ELEMENT'); //eslint-disable-line
+        path.replaceWith(
+          generateElement(path)
+        );
+        console.log(); //eslint-disable-line
+        // console.log(this);
+        // let type = path.get('openingElement').get('name').node.name;
         // let children = path.get('children').map(() => {
         //   return 'kid';
         // });
-        let attributes = path.get('openingElement').get('attributes').map(() => {
-          return 'attribute';
-        });
+        // let attributes = path.get('openingElement').get('attributes').map(() => {
+        //   return 'attribute';
+        // });
 
-        console.log(type);
+        // console.log(type);
         // console.log(children);
-        console.log(attributes);
+        // console.log(attributes);
       }
     }
   };
