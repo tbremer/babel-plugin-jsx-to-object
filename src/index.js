07b794e9354b34e4c4dfc600c455e4fa2f190466
Thomas Bremer <tom@tbremer.com>
2016-04-02 22:04:59 -0500

    remove dead code, add children creation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 export default function ({types: t}) {
   const generateElement = (path) => {
     const OPENING_ELEMENT = path.get('openingElement'),
-    elementName = OPENING_ELEMENT.node.name.name,
-    attributes = OPENING_ELEMENT.get('attributes').map((attr) => {
+    elementName = t.StringLiteral(OPENING_ELEMENT.node.name.name),
+    attributes = t.ObjectExpression(OPENING_ELEMENT.get('attributes').map((attr) => {
       return t.ObjectProperty(
         t.StringLiteral(attr.get('name').node.name),
         (attr.node.value === null ? t.BooleanLiteral(true) : attr.get('value'))
       );
-    });
+    })),
+    children = t.ArrayExpression(path.get('children').map(() => {
+      return t.StringLiteral('foo');
+    }));
 
     return t.ObjectExpression([
-      t.ObjectProperty(t.StringLiteral('elementName'), t.StringLiteral(elementName)),
-      t.ObjectProperty(t.StringLiteral('attributes'), t.ObjectExpression(attributes))
+      t.ObjectProperty(t.StringLiteral('elementName'), elementName),
+      t.ObjectProperty(t.StringLiteral('attributes'), attributes),
+      t.ObjectProperty(t.StringLiteral('children'), children)
     ]);
   };
 
@@ -19,62 +23,10 @@ export default function ({types: t}) {
     inherits: require('babel-plugin-syntax-jsx'),
     visitor: {
       JSXElement: function(path) {
-        console.log('ELEMENT'); //eslint-disable-line
         path.replaceWith(
           generateElement(path)
         );
-        console.log(); //eslint-disable-line
-        // console.log(this);
-        // let type = path.get('openingElement').get('name').node.name;
-        // let children = path.get('children').map(() => {
-        //   return 'kid';
-        // });
-        // let attributes = path.get('openingElement').get('attributes').map(() => {
-        //   return 'attribute';
-        // });
-
-        // console.log(type);
-        // console.log(children);
-        // console.log(attributes);
       }
     }
   };
 }
-
-// export default function ({types: t}) {
-//   const ELEMENT_PRAGMA = {
-//     type: String,
-//     attributes: Object,
-//     children: Array
-//   };
-
-//   const updateElement = {
-//     JSXElement: function (path) {
-//       console.log('oh, hello');
-//     }
-//     // JSXOpeningElement: function(path) {
-//       // console.log('OPENING ELEMENT SCOPE')
-//       // console.log(path.scope.parent)
-//     // },
-//     // JSXClosingElement: function() { /*console.log('closing');*/ },
-//     // JSXText: function() { /*console.log('TEXT'); */ },
-//     // JSXAttribute: function() { /*console.log('ATTR'); */},
-//   };
-
-//   return {
-//     inherits: require('babel-plugin-syntax-jsx'),
-//     visitor: {
-//       JSXElement: function(path) {
-//         console.log('ELEMENT');
-//         let ar = path.get('children').map(() => {
-//           return 'foo'
-//         });
-
-//         console.log(ar);
-//         // console.log(path.scope.parent);
-//         // path.traverse(updateElement);
-//         /*console.log("AFTER ALL"); */
-//       }
-//     }
-//   }
-// }
